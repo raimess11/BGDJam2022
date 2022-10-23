78a6785d6b19042c9a20a8aa0fabf899ad57bb89
Fakhrul0121 <106474317+Fakhrul0121@users.noreply.github.com>
2022-10-23 10:45:23 +0700

    fixing

---
+++ ui/menu.gd
@@ -0,0 +1,36 @@
+extends Control
+
+onready var vBox = $CanvasLayer/VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	vBox.add_constant_override("separation",50)
+	var start = create_tween()
+	start.tween_property($CanvasLayer/Fade,"self_modulate",Color(1,1,1,0),0.5)
+	start.call_deferred("start")
+	yield(start, "finished")
+	$CanvasLayer/Fade.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Start_pressed():
+	$CanvasLayer/Fade.visible = true
+	var fade = create_tween()
+	fade.tween_property($CanvasLayer/Fade,"self_modulate",Color(1,1,1,1),1)
+	fade.call_deferred("start")
+	yield(fade, "finished")
+	yield(get_tree().create_timer(1.0),"timeout")
+	get_tree().change_scene("res://World.tscn")
+
+
+func _on_Credit_pressed():
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
